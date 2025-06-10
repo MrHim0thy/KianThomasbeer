f5bf6dc73b3287c9eca207bd6a8e553a0fe36e0f
MrHim0thy <kayennepeppaa@gmail.com>
2025-06-09 23:20:28 -0400

    Add external CSS and JS

---
+++ script.js
@@ -0,0 +1,7 @@
+// Update footer year dynamically
+window.addEventListener('DOMContentLoaded', () => {
+  const yearSpan = document.getElementById('year');
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
